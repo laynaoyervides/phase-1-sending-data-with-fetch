008131d6d408edc188139193f232ef4862fab5a9
Layna Oyervides <laynaoyervides@gmail.com>
2021-08-27 22:37:52 -0600

    commit tree 4b8f4ba88cf5eebd9c4e8e9b112b9d2b4f393f6e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(userName, userEmail){
+    return fetch('http://localhost:3000/users', 
+        {
+            method: 'POST',
+            headers: {
+               "Content-Type": "application/json",
+               "Accept": "application/json"     
+            },
+            body: JSON.stringify(
+                {
+                    "name": userName,
+                    "email" : userEmail
+                }
+            )
+        })
+        .then (response => response.json())
+        .then(data => renderId(data))
+        .catch((error)=>{
+           document.body.innerHTML = error;
+        });
+        
+
+    }
+    const renderId = data => {
+        document.body.innerHTML = data.id;
+   
+    };
